413813005adbe0bfcc64388f4a97d21bf9011dd5
Zachary Lee <zacharylhlee@gmail.com>
2023-10-19 08:43:50 -0600

    Updated header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,12 @@
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 import Image from "next/image";
 
-const companyName = "FLC Consulting Inc.";
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const companyName = "FLC Consulting";
+const pages = ["About", "Services", "Contact Us"];
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static">
         <Toolbar>
           <Image
@@ -17,7 +16,22 @@ export default function Header() {
             width={50}
             height={50}
           />
-          <Typography textAlign="center">{companyName}</Typography>
+          <Typography variant="h5" className="m-5" textAlign="center">
+            {companyName}
+          </Typography>
+          <Box
+            className="m-5"
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ m: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
