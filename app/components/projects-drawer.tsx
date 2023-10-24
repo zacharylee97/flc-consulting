8875e6f0581ef31c144be5f85c16615d52dbd538
Zachary Lee <zacharylhlee@gmail.com>
2023-10-24 08:15:57 -0600

    Updated projects drawer

--- app/components/projects-drawer.tsx
+++ app/components/projects-drawer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Typography,
@@ -7,10 +8,15 @@ import {
   ListItem,
 } from "@mui/material";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const drawerWidth = 240;
 
+const pages = ["Optima", "Paul Teoh"];
+const links = ["/projects/optima", "/projects/paul-teoh"];
+
 export default function ProjectsDrawer() {
+  const pathname = usePathname();
   return (
     <Drawer
       variant="permanent"
@@ -26,22 +32,23 @@ export default function ProjectsDrawer() {
     >
       <Box sx={{ overflow: "auto" }}>
         <List>
-          <ListItem key={"optima"} disablePadding>
-            <Link href="/projects/optima">
-              <Typography variant="h5" className="m-5">
-                {"Optima"}
-              </Typography>
-            </Link>
-          </ListItem>
-          <Divider />
-          <ListItem key={"paul-teoh"} disablePadding>
-            <Link href="/projects/paul-teoh">
-              <Typography variant="h5" className="m-5">
-                {"Paul Teoh Chartered Accountant"}
-              </Typography>
-            </Link>
-          </ListItem>
-          <Divider />
+          {pages.map((page, index) => (
+            <Box key={page}>
+              <ListItem>
+                <Link href={links[index]}>
+                  <Typography
+                    variant="h5"
+                    className={`m-5 ${
+                      pathname === links[index] ? "font-bold" : ""
+                    }`}
+                  >
+                    {page}
+                  </Typography>
+                </Link>
+              </ListItem>
+              <Divider />
+            </Box>
+          ))}
         </List>
       </Box>
     </Drawer>
