322f150762d0de3e0f6a22d269e1db49ae12e9a1
Zachary Lee <zacharylhlee@gmail.com>
2023-10-24 08:04:28 -0600

    Added Paul Teoh project page

--- app/components/projects-drawer.tsx
+++ app/components/projects-drawer.tsx
@@ -28,8 +28,16 @@ export default function ProjectsDrawer() {
         <List>
           <ListItem key={"optima"} disablePadding>
             <Link href="/projects/optima">
-              <Typography variant="h5" className="m-5" textAlign="center">
+              <Typography variant="h5" className="m-5">
                 {"Optima"}
+              </Typography>
+            </Link>
+          </ListItem>
+          <Divider />
+          <ListItem key={"paul-teoh"} disablePadding>
+            <Link href="/projects/paul-teoh">
+              <Typography variant="h5" className="m-5">
+                {"Paul Teoh Chartered Accountant"}
               </Typography>
             </Link>
           </ListItem>
