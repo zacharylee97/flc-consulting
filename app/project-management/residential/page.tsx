4a234f71d5b921684ece4720c4386b52fbf824e5
Zachary Lee <zacharylhlee@gmail.com>
2023-10-30 16:05:32 -0600

    Added Rocky Ridge to Residential page

--- app/project-management/residential/page.tsx
+++ app/project-management/residential/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/components/header";
 import { Box, Typography } from "@mui/material";
+import Image from "next/image";
 
 const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Aliquam ipsum risus, dapibus iaculis massa vitae, consectetur mattis dolor. 
@@ -8,7 +9,7 @@ Vestibulum et diam accumsan, volutpat quam vitae, mollis tellus. Sed porta
 fringilla vehicula. Nunc odio justo, bibendum nec arcu facilisis, ultricies 
 congue nulla. Suspendisse potenti.`;
 
-export default function Industrial() {
+export default function Residential() {
   return (
     <main className="min-h-screen">
       <Header></Header>
@@ -18,6 +19,23 @@ export default function Industrial() {
       </Box>
       <Box className="m-10">
         <Typography variant="h6">{text}</Typography>
+        <Typography className="mt-5" variant="h5">
+          Rocky Ridge
+        </Typography>
+        <Image
+          className="m-5"
+          src="/RockyRidge1.png"
+          alt="Rocky Ridge 1"
+          width={500}
+          height={50}
+        />
+        <Image
+          className="m-5"
+          src="/RockyRidge2.png"
+          alt="Rocky Ridge 2"
+          width={500}
+          height={50}
+        />
       </Box>
     </main>
   );
