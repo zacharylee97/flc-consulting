9a8f9054b19c3b0eabe8064c33bf77c35452735e
Zachary Lee <zacharylhlee@gmail.com>
2023-10-30 16:00:38 -0600

    Created Project Management pages

---
+++ app/project-management/commercial/page.tsx
@@ -0,0 +1,24 @@
+import Header from "@/app/components/header";
+import { Box, Typography } from "@mui/material";
+
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Aliquam ipsum risus, dapibus iaculis massa vitae, consectetur mattis dolor. 
+Aenean feugiat augue varius ipsum accumsan tempor. Morbi in felis nibh. 
+Vestibulum et diam accumsan, volutpat quam vitae, mollis tellus. Sed porta 
+fringilla vehicula. Nunc odio justo, bibendum nec arcu facilisis, ultricies 
+congue nulla. Suspendisse potenti.`;
+
+export default function Commercial() {
+  return (
+    <main className="min-h-screen">
+      <Header></Header>
+      <Box className="mx-10 mt-28">
+        <Typography variant="h1">Project Management</Typography>
+        <Typography variant="h3">Commercial</Typography>
+      </Box>
+      <Box className="m-10">
+        <Typography variant="h6">{text}</Typography>
+      </Box>
+    </main>
+  );
+}
